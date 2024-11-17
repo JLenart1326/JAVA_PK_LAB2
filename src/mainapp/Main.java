4034777d8645ed76132f427e16e8acd6d02d400c
JLenart1326 <75327126+JLenart1326@users.noreply.github.com>
2024-11-17 08:43:05 +0100

    Lab2

---
+++ src/mainapp/Main.java
@@ -0,0 +1,14 @@
+package mainapp;
+
+public class Main {
+    public static void main(String[] args) {
+        Color color_1 = new Color();
+        Color color_2 = new Color(145,255,15);
+        Color color_3 = new Color(255,234,25,143);
+        //Color color_4 = new Color(453,-14,-34,777);
+
+        color_1.print();
+        color_2.print();
+        color_3.print();
+    }
+}
